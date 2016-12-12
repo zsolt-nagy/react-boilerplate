822d69426a908a44ce90523c541c26470a60efe9
zsolt-nagy <info@zsoltnagy.eu>
2016-12-12 23:41:42 +0100

    Add Clear to MessageList

--- src/app/components/MessageList.js
+++ src/app/components/MessageList.js
@@ -2,15 +2,35 @@ import React from 'react';
 import Message from './Message';
 
 class MessageList extends React.Component {
+    constructor( props, context ) {
+        super( props, context );
+        this.state = {
+            isClearDisabled: false
+        };
+
+        this.handleClearMessages = this.handleClearMessages.bind( this );
+    }
+    handleClearMessages() {
+        this.setState({
+            isClearDisabled: true
+        });        
+    }
     render() {
+        let button = <button onClick={this.handleClearMessages}>Clear</button>;
+        if ( this.state.isClearDisabled ) {
+            button = <button disabled="disabled">Clear</button>;
+        }
         return (
             <div>
-                { this.props.messages.map( message => 
-                    <Message timestamp={message.timestamp}
-                             owner={message.owner}
-                             text={message.text}
-                             key={message.id} />
-                  )}
+                { this.state.isClearDisabled ? [] :
+                      this.props.messages.map( message => 
+                        <Message timestamp={message.timestamp}
+                                 owner={message.owner}
+                                 text={message.text}
+                                 key={message.id} />
+                      )
+                }
+                {button}
             </div>
         );
     }
